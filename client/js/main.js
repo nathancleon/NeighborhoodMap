d0eb8f13a7e429315dbc98cdaaa55d849a3efaff
Nathaniel Collins <nathancleon@gmail.com>
2016-06-15 20:47:53 -0500

    Google Maps API setup

---
+++ client/js/main.js
@@ -0,0 +1,7 @@
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map-canvas'), {
+          center: {lat: 30.2672, lng: -97.7431},
+          zoom: 12
+        });
+      }
