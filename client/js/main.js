d0f621d8cd07038c54b22b5058ff3de6917787d9
Nathaniel Collins <nathancleon@gmail.com>
2016-09-29 19:02:32 -0500

    Attempting to place markers

--- client/js/main.js
+++ client/js/main.js
@@ -1,7 +1,26 @@
 var map;
       function initMap() {
-        map = new google.maps.Map(document.getElementById('map-canvas'), {
-          center: {lat: 30.2672, lng: -97.7431},
-          zoom: 12
+        map = new google.maps.Map(document.getElementById("map-canvas"), {
+          zoom: 12,
+          center: new google.maps.LatLng(30.2672,-97.7431),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
         });
+
+        var infoWindow = new google.maps.InfoWindow;
+
+        var marker, i;
+
+        for (i = 0; i < coffeeData.length; i++) {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(coffeeData[i][1], coffeeData[i][2]),
+            map: map
+          });
+
+          google.maps.event.addListener(marker, "click", (function(marker, i) {
+            return function() {
+              infoWindow.setContent(coffeeData[i][0]);
+              infoWindow.open(map, marker);
+            }
+          })(marker, i));
+        }
       }
