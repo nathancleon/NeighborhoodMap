edf20bc7647712985e7dc0886a63ecf1a932c058
Nathaniel Collins <nathancleon@gmail.com>
2016-10-11 21:02:13 -0500

    coffee shop array
    array file updated with 2 different options

--- client/js/coffee-data.js
+++ client/js/coffee-data.js
@@ -1,66 +1,78 @@
-// var coffeeData = [
-//   { 'title': 'Summer Moon Coffee',
-//     'lat': '30.3886432',
-//     'lng': '-97.8392678'
-//   },
-//   { 'title': 'Epoch Coffee',
-//     'lat': '30.3185636',
-//     'lng': '-97.7267032'
-//   },
-//   { 'title': 'Epoch Coffee at Anderson Lane',
-//     'lat': '30.359334',
-//     'lng': '-97.7367637'
-//   },
-//   { 'title': 'Mozarts Coffee Roasters',
-//     'lat': '30.2954206',
-//     'lng': '-97.7864584'
-//   },
-//   { 'title': 'Jo\'s Coffee',
-//     'lat': '30.2510427',
-//     'lng': '-97.7668808'
-//   },
-//   { 'title': 'Thunderbird Coffee',
-//     'lat': '30.3307876',
-//     'lng': '-97.7340764'
-//   },
-//   { 'title': 'Coffee Bean & Tea Leaf',
-//     'lat': '30.3307689',
-//     'lng': '-97.8019279'
-//   },
-//   { 'title': 'Bennu Coffee',
-//     'lat': '30.2797982',
-//     'lng': '-97.7217717'
-//   },
-//   { 'title': 'Genuine Joe\'s Coffeehouse',
-//     'lat': '30.3540797',
-//     'lng': '-97.7294721'
-//   },
-//   { 'title': 'Quack\'s 43rd St Bakery',
-//     'lat': '30.304645',
-//     'lng': '-97.7287721'
-//   },
-//   { 'title': 'Blue Cat Cafe',
-//     'lat': '30.258808',
-//     'lng': '-97.7327257'
-//   },
-//   { 'title': 'Dominican Joe Coffee Shop',
-//     'lat': '30.256043',
-//     'lng': '-97.7484887'
-//   },
-// ];
-
 var coffeeData = [
-      ['<strong>Mozart\'s Coffee Roasters</strong>', 30.295420, -97.784269],
-      ['<strong>Summer Moon Coffee</strong>', 30.2332635, -97.7670598],
-      ['<strong>Coffee Bean & Tea Leaf</strong>', 30.2998368, -97.7240849],
-      ['<strong>Epoch Coffee</strong>', 30.3185636, -97.7267032]
-      ['<strong>Epoch Coffee at Anderson Lane</strong>', 30.359334, -97.7367637],
-      ['<strong>Mozarts Coffee Roasters</strong>', 30.2954206, -97.7864584],
-      ['<strong>Jo\'s Coffee</strong>', 30.2510427, -97.7668808],
-      ['<strong>Thunderbird Coffee</strong>', 30.3307876, -97.7340764],
-      ['<strong>Bennu Coffee</strong>', 30.2797982, -97.7217717],
-      ['<strong>Genuine Joe\'s Coffeehouse</strong>', 30.3540797, -97.7294721],
-      ['<strong>Quack\'s 43rd St Bakery</strong>', 30.304645, -97.7287721],
-      ['<strong>Blue Cat Cafe</strong>', 30.258808, -97.7327257],
-      ['<strong>Dominican Joe Coffee Shop</strong>', 30.256043, -97.7484887]
-    ];
+  { title: 'Summer Moon Coffee',
+    lat: '30.3886432',
+    lng: '-97.8392678',
+    address: '3115 S 1st St #1B, Austin, TX 78704'
+  },
+  { title: 'Epoch Coffee',
+    lat: '30.3185636',
+    lng: '-97.7267032',
+    address: '221 W N Loop Blvd, Austin, TX 78751'
+  },
+  { title: 'Epoch Coffee at Anderson Lane',
+    lat: '30.359334',
+    lng: '-97.7367637',
+    address: '2700 W Anderson Ln #409, Austin, TX 78757'
+  },
+  { title: 'Mozarts Coffee Roasters',
+    lat: '30.2954206',
+    lng: '-97.7864584',
+    address: '3825 Lake Austin Blvd, Austin, TX 78703'
+  },
+  { title: 'Jo\'s Coffee',
+    lat: '30.2512945',
+    lng: '-97.7966038',
+    address: '1300 S Congress Ave, Austin, TX 78704',
+  },
+  { title: 'Thunderbird Coffee',
+    lat: '30.3307876',
+    lng: '-97.7340764',
+    address: '1401 W Koenig Ln, Austin, TX 78756'
+  },
+  { title: 'Coffee Bean & Tea Leaf',
+    lat: '30.3307689',
+    lng: '-97.8019279',
+    address: 'Hancock Center, 1000 E 41st St #100, Austin, TX 78751'
+  },
+  { title: 'Bennu Coffee',
+    lat: '30.2797982',
+    lng: '-97.7217717',
+    address: '2001 E Martin Luther King Jr Blvd, Austin, TX 78702'
+  },
+  { title: 'Genuine Joe\'s Coffeehouse',
+    lat: '30.3540797',
+    lng: '-97.7294721',
+    address: '2001 W Anderson Ln, Austin, TX 78757'
+  },
+  { title: 'Quack\'s 43rd St Bakery',
+    lat: '30.304645',
+    lng: '-97.7287721',
+    address: '411 E 43rd St, Austin, TX 78751'
+  },
+  { title: 'Blue Cat Cafe',
+    lat: '30.258808',
+    lng: '-97.7327257',
+    address: '95 Navasota St, Austin, TX 78702'
+  },
+  { title: 'Dominican Joe Coffee Shop',
+    lat: '30.256043',
+    lng: '-97.7484887',
+    address: '515 S Congress Ave, Austin, TX 78704'
+  },
+];
+
+// var coffeeData = [
+//       ['<strong>Mozart\'s Coffee Roasters</strong>', 30.295420, -97.784269],
+//       ['<strong>Summer Moon Coffee</strong>', 30.2332635, -97.7670598],
+//       ['<strong>Coffee Bean & Tea Leaf</strong>', 30.2998368, -97.7240849],
+//       ['<strong>Epoch Coffee</strong>', 30.3185636, -97.7267032]
+//       ['<strong>Epoch Coffee at Anderson Lane</strong>', 30.359334, -97.7367637],
+//       ['<strong>Mozarts Coffee Roasters</strong>', 30.2954206, -97.7864584],
+//       ['<strong>Jo\'s Coffee</strong>', 30.2510427, -97.7668808],
+//       ['<strong>Thunderbird Coffee</strong>', 30.3307876, -97.7340764],
+//       ['<strong>Bennu Coffee</strong>', 30.2797982, -97.7217717],
+//       ['<strong>Genuine Joe\'s Coffeehouse</strong>', 30.3540797, -97.7294721],
+//       ['<strong>Quack\'s 43rd St Bakery</strong>', 30.304645, -97.7287721],
+//       ['<strong>Blue Cat Cafe</strong>', 30.258808, -97.7327257],
+//       ['<strong>Dominican Joe Coffee Shop</strong>', 30.256043, -97.7484887]
+//     ];
